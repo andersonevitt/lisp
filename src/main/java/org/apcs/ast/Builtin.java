31fcba3ab7f8341ec13db5a2f4b3ab1b70d8ac21
andersonevitt <abcmontey@gmail.com>
2023-05-17 10:00:21 -0400

    Comment Builtin.java

--- src/main/java/org/apcs/ast/Builtin.java
+++ src/main/java/org/apcs/ast/Builtin.java
@@ -5,9 +5,22 @@ import org.apcs.core.Environment;
 import java.util.List;
 
 public interface Builtin extends Value {
+    /**
+     * Call the current builtin with the given environment and argumments
+     *
+     * @param env  the current environment to use
+     * @param args the arguments to the function
+     * @return the evaluated builtin function result
+     */
     Value apply(Environment env, List<Value> args);
 
-    default Object value() {
+    /**
+     * The name of the builtin as a string.
+     * This should not be implemented if the builtin has the @Define annotation
+     *
+     * @return the name of the function
+     */
+    default String value() {
         return null;
     }
 }
