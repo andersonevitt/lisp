4d762830ff8a6930680fc74f1242d31322c728b7
andersonevitt <abcmontey@gmail.com>
2023-05-19 10:50:54 -0400

    Add comments

--- src/main/java/org/apcs/core/Equals.java
+++ src/main/java/org/apcs/core/Equals.java
@@ -10,6 +10,14 @@ import java.util.List;
 
 @Define("=")
 public class Equals implements BuiltinValue {
+    /**
+     * Compares two values for equality
+     *
+     * @param env  the current environment to use
+     * @param args the arguments to the function
+     * @return true if the two arguments are equal, false otherwise
+     * @throws LispException if the arguments fail to evaluate
+     */
     @Override
     public Value<?> apply(Environment env, List<Value<?>> args) throws LispException {
         return new BoolValue(args.get(0).eval(env).equals(args.get(1).eval(env)));
--- src/main/java/org/apcs/core/GT.java
+++ src/main/java/org/apcs/core/GT.java
@@ -9,6 +9,13 @@ import static org.apcs.core.CoreUtils.cast;
 
 @Define(">")
 public class GT implements BuiltinValue {
+    /**
+     * Returns true if the first argument is greater than the second argument
+     *
+     * @param env  the current environment to use
+     * @param args the arguments to the function
+     * @return whether the first argument is greater than the second
+     */
     @Override
     public Value<?> apply(Environment env, List<Value<?>> args) throws LispException {
         return new BoolValue(cast(args.get(0).eval(env), NumberValue.class) > cast(args.get(1).eval(env),
