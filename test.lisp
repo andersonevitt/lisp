765039bbbbc5e26dd23ef094bb184c4b8a626656
andersonevitt <abcmontey@gmail.com>
2023-05-19 01:05:09 -0400

    Added demo for tomorrow.

--- test.lisp
+++ test.lisp
@@ -1,4 +1,21 @@
 
-(def f (lambda (x) (+ x 10 20)))
+(println "hello world")
 
-(+ "some" 10)
+(def nil '())
+
+(def a-list '(122 23 34 321))
+
+; List operations
+(println (first a-list))
+(println (rest a-list))
+
+(def f (lambda (x) (* x 10 20)))
+
+(println (+ 20 12 10 (f 100)))
+
+(defun fact (n)
+    (if (= n 0)
+        1
+        (* n (fact (- n 1)))))
+
+(println (fact 5))
